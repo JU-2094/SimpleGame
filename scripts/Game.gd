00b5bd12d37b54a946206e54d920f63e2b656827
JU-2094 <urbgon@gmail.com>
2019-05-26 18:24:24 -0500

    platform

--- scripts/Game.gd
+++ scripts/Game.gd
@@ -10,10 +10,11 @@ export var ptype ="normal"
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var temp="bouncy"
-	#$Platform.init_plat(temp)
+	# $Platform.init_plat(temp)
 	var ptype ="normal"
 	monster = load('res://scenes/Monster.tscn').instance()
 	add_child(monster)
+	
 	print(monster.name)
 	pass # Replace with function body.
 
