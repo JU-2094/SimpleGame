df9fb3b042fcb1f59858df8fcf605e90a5e68c8c
MrN3m0 <mendezluceromiguelangel@gmail.com>
2019-05-28 16:23:16 -0500

    added platforms added particles

--- Platform.gd
+++ Platform.gd
@@ -8,7 +8,7 @@ var tbouncy =preload("res://assets/PNG/shiny/11.png")
 var tsticky =preload("res://assets/PNG/shiny/8.png")
 var tdead = preload("res://assets/PNG/shiny/10.png")
 var tice = preload("res://assets/PNG/shiny/9.png")
-export var ptype ="normal"
+var dptype ={1:"normal",2:"bouncy",3:"sticky",4:"ice",5:"dead"}
 var deadly =false
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -17,29 +17,28 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-func init_plat(type):
-	ptype=type
-	if ptype == "normal":
+func init_plat(num):
+	if dptype[num] == "normal" :
 		self.physics_material_override.friction=1
 		self.physics_material_override.bounce=10
 		self.physics_material_override.absorbent=false
 		$CollisionShape2D/Sprite.texture=tnormal
-	if ptype == "bouncy":
+	if dptype[num] == "bouncy" :
 		self.physics_material_override.friction=1
 		self.physics_material_override.bounce=70
 		self.physics_material_override.absorbent=false
 		$CollisionShape2D/Sprite.texture=tbouncy
-	if ptype == "sticky":
+	if dptype[num] == "sticky" :
 		self.physics_material_override.friction=4
 		self.physics_material_override.bounce=0
 		self.physics_material_override.absorbent=true
 		$CollisionShape2D/Sprite.texture=tsticky
-	if ptype == "ice":
+	if dptype[num] == "ice" :
 		self.physics_material_override.friction=-10
 		self.physics_material_override.bounce=0
 		self.physics_material_override.absorbent=false
 		$CollisionShape2D/Sprite.texture=tice
-	if ptype == "dead":
+	if dptype[num] == "dead":
 		self.physics_material_override.friction=1
 		self.physics_material_override.bounce=0
 		self.physics_material_override.absorbent=false
--- scripts/Game.gd
+++ scripts/Game.gd
@@ -5,18 +5,50 @@ extends Node2D
 # var b = "text"
 var a
 var monster
+var prev_pos
+var curr_pos
+var platforml
+var platformr
+var randl = RandomNumberGenerator.new()
+var randr = RandomNumberGenerator.new()
 var psize = Vector2(1,1)
-export var ptype ="normal"
+var PLATFORM = preload('res://scenes/Platform.tscn')
+var width = ProjectSettings.get_setting("display/window/size/width")
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var temp="bouncy"
-	$Platform.init_plat(temp)
-	var ptype ="normal"
+	$Platform.init_plat(2)
 	monster = load('res://scenes/Monster.tscn').instance()
 	add_child(monster)
+	prev_pos=monster.position.y
+	curr_pos=monster.position.y
+	#print("here",prev_pos)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	#print(plat.transform)
+	ppos()
+	
+	pass
+func ppos():
+	curr_pos=monster.position.y
+	print(curr_pos,":",prev_pos)
+	if curr_pos-prev_pos <=-200:
+		prev_pos=curr_pos
+		#print("papu",prev_pos)
+		pgen()
+	pass
+func pgen():
+	randl.randomize()
+	randr.randomize()
+	var pltype=randl.randi_range(1,5)
+	var prtype=randr.randi_range(1,5)
+	platforml = PLATFORM.instance()
+	platforml.init_plat(pltype)
+	platformr = PLATFORM.instance()
+	platformr.init_plat(prtype)
+	platforml.global_position = Vector2(0,curr_pos)
+	platformr.global_position = Vector2(500,curr_pos)
+	add_child(platforml)
+	add_child(platformr)
 	pass
